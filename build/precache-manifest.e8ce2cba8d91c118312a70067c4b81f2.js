e3ae65552f0727df311eaa37f93d3f0a12672561
Sergei <SPetrenko17@yandex.ru>
2020-08-04 21:41:11 +0300

    init

---
+++ build/precache-manifest.e8ce2cba8d91c118312a70067c4b81f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aabce5807771479d0f92fcf7c306a85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9619620637155b9c058",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe9628a62c7dfb34270f",
+    "url": "/static/js/2.707f1bfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.707f1bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9619620637155b9c058",
+    "url": "/static/js/main.a46b4d40.chunk.js"
+  },
+  {
+    "revision": "4178a590d621709143ec",
+    "url": "/static/js/runtime-main.40c3c141.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
